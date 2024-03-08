17d76b4fb0f0e0993781c6cb975434e9b3e49277
Qarola <cyan.323@gmail.com>
2024-03-08 16:47:56 -0300

    initial commit

---
+++ server/index.js
@@ -0,0 +1,69 @@
+const dotenv = require("dotenv");
+const express = require("express");
+const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+
+const app = express();
+dotenv.config(); 
+
+// Función para conectar a MongoDB
+const connectToMongoDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI);
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+    throw error;
+  }
+};
+
+// Manejar desconexión de MongoDB al detener o reiniciar el servidor
+mongoose.connection.on("disconnected", () => {
+  console.log("Disconnected from MongoDB");
+});
+process.on("SIGINT", async () => {
+  try {
+    await mongoose.connection.close();
+    console.log("Disconnected from MongoDB");
+    process.exit(0);
+  } catch (error) {
+    console.error("Error disconnecting from MongoDB", error);
+    process.exit(1);
+  }
+});
+
+// Middlewares
+const corsOptions = {
+  origin: "*",
+  methods: "GET,POST,PUT,DELETE",
+};
+
+app.use(cors(corsOptions)); // Configurar CORS
+app.use(cookieParser()); // Analizar cookies
+app.use(express.json()); // Analizar solicitudes JSON
+
+//Ruta de prueba para verificar el estado del servidor remoto
+/* app.get("/health", (req, res) => {
+  res.status(200).json({ message: "OK" });
+}); */
+
+// Middleware para manejar errores
+app.use((err, req, res, next) => {
+  const errorStatus = err.status || 500;
+  const errorMessage = err.message || "Something went wrong!";
+  return res.status(errorStatus).json({
+    success: false,
+    status: errorStatus,
+    message: errorMessage,
+    stack: err.stack,
+  });
+});
+
+const port = process.env.PORT || 5000;
+
+// Iniciar servidor y conectar a MongoDB
+app.listen(port, () => {
+  connectToMongoDB();
+  console.log(`Server running on port ${port}`);
+});
