139519d4371982a634d3967c32ea4a6f015da0e3
Qarola <cyan.323@gmail.com>
2024-03-14 20:24:20 -0300

    add GET route to getPropertyDetails

--- server/routes/propertyRoutes/property.js
+++ server/routes/propertyRoutes/property.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const {getProperties} = require('../../controllers/propertyController/propertyController')
+const {getProperties, getPropertyDetails} = require('../../controllers/propertyController/propertyController')
 
 
 //GET
 router.get("/properties", getProperties);
+router.get("/detail/:id", getPropertyDetails);
 
 module.exports = router;
