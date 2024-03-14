208729abcb795508c5f5442ee33185c4e5716947
Qarola <cyan.323@gmail.com>
2024-03-14 18:47:59 -0300

    server/

--- server/routes/propertyRoutes/property.js
+++ server/routes/propertyRoutes/property.js
@@ -1,14 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { getProperties, getPropertyDetails } = require('../../controllers/propertyController/propertyController')
+const {getProperties} = require('../../controllers/propertyController/propertyController')
 
 
 //GET
 router.get("/properties", getProperties);
-router.get('/propiedades/:id', getPropertyDetails);
-
-
-
-
 
 module.exports = router;
