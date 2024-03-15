abb6942067488b3b5bb447c9a81f391ecef7a141
Qarola <cyan.323@gmail.com>
2024-03-15 12:16:55 -0300

    add POST route to contactRealEstate controller

--- server/routes/propertyRoutes/property.js
+++ server/routes/propertyRoutes/property.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const {getProperties, getPropertyDetails} = require('../../controllers/propertyController/propertyController')
+const { getProperties, getPropertyDetails, contactRealEstate } = require('../../controllers/propertyController/propertyController')
 
 
 //GET
 router.get("/properties", getProperties);
 router.get("/detail/:id", getPropertyDetails);
+
+//POST
+router.post("/contactar-inmobiliaria", contactRealEstate);
 
 module.exports = router;
