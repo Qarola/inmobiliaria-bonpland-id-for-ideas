d5f4c3716b44b89b3e25f1f0f5103c81e33f61f2
Ariel Barrios <ariel29111978@hotmail.com>
2024-03-22 23:53:47 -0300

    add BrowserRouter and Routes in the index

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,16 @@
 import './App.css'
+import { BrowserRouter, Routes } from 'react-router-dom';
 
 function App() {
 
   return (
-    <h1 className="text-3xl font-bold underline">Hola mundo</h1>
+
+    <BrowserRouter>
+      <h1 className="text-3xl font-bold underline">Hola mundo</h1>
+      <Routes>
+        {/* Rutas */}
+      </Routes>
+    </BrowserRouter>
   )
 }
 
