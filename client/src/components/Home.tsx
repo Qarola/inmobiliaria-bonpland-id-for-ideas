7bae90048021384a5c8710335761a4d5f4a23e2e
Ariel Barrios <ariel29111978@hotmail.com>
2024-03-25 17:44:31 -0300

    Add TeanCard

--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -1,9 +1,13 @@
 import Component_6 from './Globals/Component_6';
+import { TeanCard } from './TeanCard/TeanCard';
+
+import oliviaRhye from '../assets/images/Olivia Rhye.png'
 
 const Home = () => {
-    return(
+    return (
         <div className="">
-            <Component_6/>
+            <Component_6 />
+            <TeanCard image={oliviaRhye} name={'Olivia Rhye'} role={'Founder & CEO'} jobDescription={'Former co-founder of Opendoor. Early staff at Spotify and Clearbit.'} />
         </div>
     )
 }
---
+++ client/src/components/TeanCard/TeanCard.tsx
@@ -0,0 +1,25 @@
+import { SocialNetworkEnum, IconColorEnum, SocialMediaComponent } from "../SocialMediaComponent/SocialMediaComponent";
+
+interface params {
+  image: string;
+  name: string;
+  role: string;
+  jobDescription: string;
+}
+
+export const TeanCard: (params: params) => JSX.Element = (params: params) => {
+  return (
+    <article className='max-w-[280px] px-12 flex flex-col items-center'>
+      <img src={params.image} className='m-4 rounded-full w-[80px]' />
+      <section className='text-center'>
+        <div className='my-2'>
+          <p className='font-bold'>{params.name}</p>
+          <p className='text-[#C9CED4]'>{params.role}</p>
+
+        </div>
+        <p className='text-center text-[#475467]'>{params.jobDescription}</p>
+        <SocialMediaComponent className='flex justify-center gap-2 py-4' socialNetworks={[SocialNetworkEnum.facebook, SocialNetworkEnum.instagram, SocialNetworkEnum.twitter]} color={IconColorEnum.black} size={24} />
+      </section>
+    </article>
+  )
+}
