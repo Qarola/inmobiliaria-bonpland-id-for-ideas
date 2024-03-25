785bc2f2bfd4a505a8dc1683fe15878cd27ac1ed
francogarcia02 <francoalbertogarcia2017@gmail.com>
2024-03-25 18:36:57 -0300

    Develop update

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,11 +1,17 @@
 import Buscador from '../components/Globals/Buscador';
 import Component_6 from '../components/Globals/Component_6';
+import { TeanCard } from '../components/TeanCard/TeanCard';
+
+import oliviaRhye from '../assets/images/Olivia Rhye.png'
 
 const Home = () => {
+
+
     return(
         <div className="">
             <Buscador/>
             <Component_6/>
+            <TeanCard image={oliviaRhye} name={'Olivia Rhye'} role={'Founder & CEO'} jobDescription={'Former co-founder of Opendoor. Early staff at Spotify and Clearbit.'} />
         </div>
     )
 }
