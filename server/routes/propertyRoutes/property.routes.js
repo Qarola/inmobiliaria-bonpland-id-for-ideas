c39ce8ad89b164e16f49a53667da3097ee9fb601
miusarname2 <oscarmauricio631@gmail.com>
2024-03-26 11:37:49 -0500

    feat: :sparkles: Added a real estate search controller with different filters, along with their respective routes.

--- server/routes/propertyRoutes/property.routes.js
+++ server/routes/propertyRoutes/property.routes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const property = express.Router();
-const { getProperties, getPropertiesList, changePropertyStatus, searchPropertiesByType, getFeaturedProperties, getPropertyDetails, contactRealEstate,EditProperty,deleteProperty } = require('../../controllers/propertyController/propertyController')
+const { getProperties,showPropertiesByFilters, getPropertiesList, changePropertyStatus, searchPropertiesByType, getFeaturedProperties, getPropertyDetails, contactRealEstate,EditProperty,deleteProperty } = require('../../controllers/propertyController/propertyController')
 
 
 //GET
@@ -9,6 +9,7 @@ property.get("/detail/:id", getPropertyDetails);
 property.get("/featured-properties", getFeaturedProperties);
 property.get("/all-properties", getPropertiesList);
 property.get("/properties/type/:propertyType", searchPropertiesByType);
+property.get("/properties/search", showPropertiesByFilters);
 
 //POST
 property.post("/contact-real-estate", contactRealEstate);
