d61ca99577870290033698b42c1d519dcc13217a
francogarcia02 <francoalbertogarcia2017@gmail.com>
2024-04-01 21:55:31 -0300

    Register update

--- client/src/pages/Register.tsx
+++ client/src/pages/Register.tsx
@@ -3,9 +3,9 @@ import RegisterForm from '../components/Forms/RegisterForm';
 
 const Register = () => {
     return(
-        <div className="flex w-[100%] h-[80hv] justify-between items-center">
+        <div className="flex w-[100%] h-[100%] justify-between items-center overflow-hidden">
             <RegisterForm/>
-            <img className="w-[40%]" src="assets/images/Register-img.png" alt="img"/>
+            <img className="w-[50%]" src="assets/images/Register-img.png" alt="img"/>
         </div>
     )
 }
