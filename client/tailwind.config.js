e1276160b89fecb0a1c047d9fed8acc160e605d7
Ariel Barrios <ariel29111978@hotmail.com>
2024-04-05 16:53:33 -0300

    style: add colors at tailwind.config.js

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -5,7 +5,19 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'blue1': '#1A3670',
+        'blue2': '#003DA2',
+        'blue3': '#4054B0',
+        'blue4': '#E8EDFF',
+        'white': '#F6F6F6',
+        'red': '#CA2D2D',
+        'black': '#212529',
+        'greyDark': '#697077',
+        'greyLigth': '#D5DAE1',
+      }
+    },
   },
   plugins: [],
 }
