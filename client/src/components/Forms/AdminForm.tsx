d05c26c8ba636de578d337aa34b52208508b674e
francogarcia02 <francoalbertogarcia2017@gmail.com>
2024-04-08 19:26:28 -0300

    add Admin log

--- client/src/components/Forms/AdminForm.tsx
+++ client/src/components/Forms/AdminForm.tsx
@@ -2,22 +2,23 @@ import { useForm } from 'react-hook-form';
 import { useState } from 'react';
 import {Link} from 'react-router-dom'
 
-const LoginForm = () => {
+const AdminForm = () => {
     const { register, handleSubmit } = useForm()
     const [data, setData] = useState({})
 
     const enviar = (d) => {
-        setData(d)
+        const user = {...d, role: 'admin'}
+        setData(user)
     }
 
     return(
-        <div className="w-[100%] flex flex-col justify-center items-center p-5">
+        <div className="w-[100%] flex flex-col justify-center items-center">
             <img src="assets/logos/MAX2.png" alt="logo" />
             <div className="flex justify-center items-center gap-4">
-                <h1 className="font-bold text-2xl lg:text-3xl">Ingresa a</h1>
+                <h1 className="font-bold text-xl lg:text-3xl">Nuevo admin en</h1>
                 <img className="w-[130px] mt-1" src="assets/logos/MAX.png" alt="logo" />
             </div>
-            <form className="p-4 flex flex-col justify-center items-center" onSubmit={handleSubmit(enviar)}>
+            <form className="w-[100%] lg:w-[50%] p-4 flex flex-col justify-center items-center" onSubmit={handleSubmit(enviar)}>
                 <div className="w-[100%]">
                     <p className="p-2 font-bold">Usuario:</p>
                     <input className="border solid gray p-2 rounded-lg w-[100%]"  placeholder="Nombre de usuario" type="text" {...register("username")}/>
@@ -30,25 +31,10 @@ const LoginForm = () => {
                     <p className="p-2 font-bold">Contraseña:</p>
                     <input className="border solid gray p-2 rounded-lg w-[100%]"  placeholder="Contraseña" type="password" {...register("password")}/>
                 </div>
-                <div className="flex justify-between items-center w-[100%] mb-5 mt-4">
-                    <div className="flex justify-start items-center gap-1">
-                        <input type="checkbox"/>
-                        <p className="text-sm font-bold">recordarme</p>
-                    </div>
-                    <Link className="text-sm text-gray-500" to="/">Olvidaste tu contraseña?</Link>
-                </div>
                 <button className="w-[100%] font-bold text-xl p-2 m-2 bg-[#1A3670] text-white rounded-lg " type="submit">Ingresar</button>
-                <button className="flex justify-center items-center text-center gap-2 w-[100%] font-bold p-2 m-2 text-[#556987] border solid gray rounded-lg">
-                    <img className="w-[30px]" src="assets/iconos/google.png" alt="Google Logo"/>
-                    Ingresar con Google
-                </button>
             </form>
-            <div className="flex justify-center items-center gap-1">
-                <p className="text-sm font-bold">Ya tienes una cuenta?</p>
-                <Link className="text-sm text-gray-500" to="/login">Ingresar</Link>
-            </div>
         </div>
     )
 }
 
-export default LoginForm
+export default AdminForm
--- client/src/pages/Register.tsx
+++ client/src/pages/Register.tsx
@@ -4,7 +4,7 @@ const Register = () => {
     return(
         <div className="flex w-[100%] h-[100%] justify-between items-center overflow-hidden">
             <RegisterForm/>
-            <img className="w-[50%]" src="assets/images/Register-img.png" alt="img"/>
+            <img className="w-[50%] search__header" src="assets/images/Register-img.png" alt="img"/>
         </div>
     )
 }
