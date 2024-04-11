372a316359d31d91f2cdfb391da12877b0a1bee6
Ariel Barrios <ariel29111978@hotmail.com>
2024-04-11 18:05:01 -0300

    refactor: clean the Home Component

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,7 +1,5 @@
 import Buscador from '../components/Buscador/Buscador';
 import Component_6 from '../components/Globals/Component_6';
-// import { TeamCard, SocialNetworkEnum } from '../components/';
-import oliviaRhye from '../assets/images/Olivia Rhye.png'
 
 import { PropertiesSection, ContactAgentsSection } from '../components';
 
@@ -14,24 +12,8 @@ const Home = () => {
     return (
         <div className="">
             <Buscador />
-            <Component_6 />
-            {/* <TeamCard image={oliviaRhye} name={'Olivia Rhye'} role={'Founder & CEO'} jobDescription={'Former co-founder of Opendoor. Early staff at Spotify and Clearbit.'} socialNetworks={[
-                {
-                    name: SocialNetworkEnum.twitter,
-                    link: 'twitter'
-                },
-                {
-                    name: SocialNetworkEnum.linkedin,
-                    link: 'linkedin'
-                },
-                {
-                    name: SocialNetworkEnum.facebook,
-                    link: 'facebook'
-                }
-            ]} /> */}
-
             <PropertiesSection properties={properties} />
-
+            <Component_6 />
             <ContactAgentsSection agentsData={agentsData} />
         </div>
     )
