8e4e5f56f7655019f9151e3daee1e56dcf063a18
Qarola <67078790+Qarola@users.noreply.github.com>
2024-04-20 00:52:00 -0300

    Update userController.js

--- server/controllers/userController/userController.js
+++ server/controllers/userController/userController.js
@@ -67,47 +67,48 @@ const login = async (req, res) => {
   try {
     const { email, password } = req.body;
 
-    // Busca al usuario por email
-    let user = await User.findOne({ email });
+    // Verificar si el correo electrónico es de un administrador
     const adminEmails = process.env.ADMIN_EMAILS.split(',');
-
-    console.log("User found:", user); // Debug log
-
-    // Si el usuario no está en la base de datos pero es el administrador definido en .env, procede con la autenticación
     if (adminEmails.includes(email)) {
       console.log("Admin login"); // Debug log
-      
-      // Genera un token JWT para el administrador
-      const token = jwt.sign({ id: user._id, role: user.role }, secretKey, { expiresIn: '1h' });
 
-      //Devuelve el token JWT como respuesta al inicio de sesión exitoso del administrador
+      // Generar un token JWT para el administrador
+      const token = jwt.sign({ email, role: 'admin' }, secretKey, { expiresIn: '1h' });
+
+      // Devolver el token JWT como respuesta al inicio de sesión exitoso del administrador
       return res.status(200).json({ message: "Inicio de sesión exitoso para el administrador", token });
     }
 
-    // Si el usuario no está en la base de datos y no es el administrador definido en .env, devuelve un error
-    if (!user && !adminEmails.includes(email)) {
+    // Si el correo electrónico no es de un administrador, buscar al usuario por correo electrónico
+    let user = await User.findOne({ email });
+
+    // Si el usuario no está en la base de datos, devolver un error
+    if (!user) {
       console.log("User not found"); // Debug log
       return res.status(404).json({ message: "Usuario no encontrado" });
     }
 
-
-    // Verifica la contraseña para usuarios normales
+    // Verificar la contraseña para usuarios normales
     const validPassword = await bcrypt.compare(password, user.password);
     if (!validPassword) {
       console.log("Invalid credentials"); // Debug log
       return res.status(401).json({ message: "Credenciales inválidas" });
     }
 
-    // Si la contraseña es válida, genera un token JWT
+    // Si la contraseña es válida, generar un token JWT
     const token = jwt.sign({ id: user._id, role: user.role }, secretKey, { expiresIn: '1h' });
 
-    // Si la contraseña es válida y el token es válido, se puede devolver un mensaje de éxito o un token de autenticación
+    // Si la contraseña es válida y el token es válido, devolver un mensaje de éxito y el token
     return res.status(200).json({ message: "Inicio de sesión exitoso", token });
   } catch (error) {
-    console.error(error); // Imprime el error en la consola para depuración
+    console.error(error); // Imprimir el error en la consola para depuración
     return res.status(500).json({ message: "Error en el servidor" });
   }
 };
+
+
+
+
 
 const getAllUsers = async (req, res) => {
   try {
