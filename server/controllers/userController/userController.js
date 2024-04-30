9e926b29e9081926049a821e3f48900bacbaa4ea
Qarola <cyan.323@gmail.com>
2024-04-30 14:02:48 -0300

    add email and password validation middleware

--- server/controllers/userController/userController.js
+++ server/controllers/userController/userController.js
@@ -1,11 +1,12 @@
 const User = require("../../models/User/User");
+const { validateEmail } = require("../../middlewares/validateEmail");
+const { validatePassword } = require("../../middlewares/validatePassword");
 const bcrypt = require("bcrypt");
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 const { validateToken } = require("../../auth/Jwt.controller");
-const dotenv = require("dotenv"); 
+const dotenv = require("dotenv");
 
 const secretKey = process.env.JWT_SECRET;
-
 
 // Carga variables de entorno desde el archivo .env
 dotenv.config();
@@ -13,25 +14,32 @@ dotenv.config();
 // Registro de usuario normal
 const registerUser = async (req, res) => {
   try {
-    const { name, email, password } = req.body;
+    validateEmail(req, res, () => {
+      validatePassword(req, res, async () => {
+        const { name, email, password } = req.body;
 
-    // Verificar si el usuario ya existe
-    let existingUser = await User.findOne({ email });
-    if (existingUser) {
-      return res.status(400).json({ message: "El usuario ya existe" });
-    }
+        // Verificar si el usuario ya existe
+        let existingUser = await User.findOne({ email });
+        if (existingUser) {
+          return res.status(400).json({ message: "El usuario ya existe" });
+        }
 
-    // Crear un nuevo usuario con el rol de usuario
-    const newUser = new User({
-      name,
-      email,
-      password: await bcrypt.hash(password, 10),
-      role: 'user', // Asigna el rol de usuario
+        // Crear un nuevo usuario con el rol de usuario
+        const newUser = new User({
+          name,
+          email,
+          password: await bcrypt.hash(password, 10),
+          role: "user", // Asigna el rol de usuario
+        });
+        await newUser.save();
+
+        return res
+          .status(201)
+          .json({ message: "Usuario registrado exitosamente" });
+      });
     });
-    await newUser.save();
-
-    return res.status(201).json({ message: "Usuario registrado exitosamente" });
   } catch (error) {
+    console.error("Error al registrar el usuario:", error.message);
     return res.status(500).json({ message: "Error en el servidor" });
   }
 };
@@ -39,25 +47,32 @@ const registerUser = async (req, res) => {
 // Registro de administrador desde el panel de administrador
 const registerAdminFromDashboard = async (req, res) => {
   try {
-    const { name, email, password } = req.body;
+    validateEmail(req, res, () => {
+      validatePassword(req, res, async () => {
+        const { name, email, password } = req.body;
 
-    // Verificar si el usuario ya existe
-    let existingUser = await User.findOne({ email });
-    if (existingUser) {
-      return res.status(400).json({ message: "El usuario ya existe" });
-    }
+        // Verificar si el usuario ya existe
+        let existingUser = await User.findOne({ email });
+        if (existingUser) {
+          return res.status(400).json({ message: "El usuario ya existe" });
+        }
 
-    // Crear un nuevo usuario con el rol de administrador
-    const newUser = new User({
-      name,
-      email,
-      password: await bcrypt.hash(password, 10),
-      role: 'admin', // Asigna el rol de administrador
+        // Crear un nuevo usuario con el rol de administrador
+        const newUser = new User({
+          name,
+          email,
+          password: await bcrypt.hash(password, 10),
+          role: "admin", // Asigna el rol de administrador
+        });
+        await newUser.save();
+
+        return res
+          .status(201)
+          .json({ message: "Administrador registrado exitosamente" });
+      });
     });
-    await newUser.save();
-
-    return res.status(201).json({ message: "Administrador registrado exitosamente" });
   } catch (error) {
+    console.error("Error al registrar el nuevo administrador:", error.message);
     return res.status(500).json({ message: "Error en el servidor" });
   }
 };
@@ -65,47 +80,61 @@ const registerAdminFromDashboard = async (req, res) => {
 // Inicio de sesión de usuario y admin
 const login = async (req, res) => {
   try {
-    const { email, password, role } = req.body;
+        const { email, password, role } = req.body;
 
-    // Verificar si el usuario intenta iniciar sesión como administrador sin serlo
-    if (role === 'admin' && !isAdmin(email)) {
-      console.log("No puedes iniciar sesión como administrador"); // Registro de depuración
-      return res.status(403).json({ message: "No tienes permisos para iniciar sesión como administrador" });
-    }
+        // Verificar si el usuario intenta iniciar sesión como administrador sin serlo
+        if (role === "admin" && !isAdmin(email)) {
+          console.log("No puedes iniciar sesión como administrador"); // Registro de depuración
+          return res.status(403).json({
+            message:
+              "No tienes permisos para iniciar sesión como administrador",
+          });
+        }
 
-    // Verificar si el correo electrónico es de un administrador
-    const adminEmails = process.env.ADMIN_EMAILS.split(',');
-    if (adminEmails.includes(email) && role === 'admin') {
-      console.log("Inicio de sesión de administrador"); // Registro de depuración
+        // Verificar si el correo electrónico es de un administrador
+        const adminEmails = process.env.ADMIN_EMAILS.split(",");
+        if (adminEmails.includes(email) && role === "admin") {
+          console.log("Inicio de sesión de administrador"); // Registro de depuración
 
-      // Generar un token JWT para el administrador con el rol 'admin'
-      const token = jwt.sign({ email, role: 'admin' }, secretKey, { expiresIn: '1h' });
+          // Generar un token JWT para el administrador con el rol 'admin'
+          const token = jwt.sign({ email, role: "admin" }, secretKey, {
+            expiresIn: "1h",
+          });
 
-      // Devolver el token JWT como respuesta al inicio de sesión exitoso del administrador
-      return res.status(200).json({ message: "Inicio de sesión exitoso para el administrador", token, role: 'admin' });
-    }
+          // Devolver el token JWT como respuesta al inicio de sesión exitoso del administrador
+          return res.status(200).json({
+            message: "Inicio de sesión exitoso para el administrador",
+            token,
+            role: "admin",
+          });
+        }
 
-    // Si el correo electrónico no es de un administrador, buscar al usuario por correo electrónico
-    let user = await User.findOne({ email });
+        // Si el correo electrónico no es de un administrador, buscar al usuario por correo electrónico
+        let user = await User.findOne({ email });
 
-    // Si el usuario no está en la base de datos, devolver un error
-    if (!user) {
-      console.log("Usuario no encontrado"); // Registro de depuración
-      return res.status(404).json({ message: "Usuario no encontrado" });
-    }
+        // Si el usuario no está en la base de datos, devolver un error
+        if (!user) {
+          console.log("Usuario no encontrado"); // Registro de depuración
+          return res.status(404).json({ message: "Usuario no encontrado" });
+        }
 
-    // Verificar la contraseña para usuarios normales
-    const validPassword = await bcrypt.compare(password, user.password);
-    if (!validPassword) {
-      console.log("Credenciales inválidas"); // Registro de depuración
-      return res.status(401).json({ message: "Credenciales inválidas" });
-    }
+        // Verificar la contraseña para usuarios normales
+        const validPassword = await bcrypt.compare(password, user.password);
+        if (!validPassword) {
+          console.log("Credenciales inválidas"); // Registro de depuración
+          return res.status(401).json({ message: "Credenciales inválidas" });
+        }
 
-    // Si la contraseña es válida, generar un token JWT con el rol del usuario
-    const token = jwt.sign({ id: user._id, role }, secretKey, { expiresIn: '1h' });
+        // Si la contraseña es válida, generar un token JWT con el rol del usuario
+        const token = jwt.sign({ id: user._id, role }, secretKey, {
+          expiresIn: "1h",
+        });
 
-    // Si la contraseña es válida y el token es válido, devolver un mensaje de éxito, el token y el rol del usuario
-    return res.status(200).json({ message: "Inicio de sesión exitoso", token, role });
+        // Si la contraseña es válida y el token es válido, devolver un mensaje de éxito, el token y el rol del usuario
+        return res
+          .status(200)
+          .json({ message: "Inicio de sesión exitoso", token, role });
+          
   } catch (error) {
     console.error(error); // Registro de errores en la consola para depuración
     return res.status(500).json({ message: "Error en el servidor" });
@@ -114,13 +143,9 @@ const login = async (req, res) => {
 
 // Función para verificar si un usuario tiene permisos de administrador
 const isAdmin = (email) => {
-  const adminEmails = process.env.ADMIN_EMAILS.split(',');
+  const adminEmails = process.env.ADMIN_EMAILS.split(",");
   return adminEmails.includes(email);
 };
-
-
-
-
 
 const getAllUsers = async (req, res) => {
   try {
@@ -138,11 +163,9 @@ const getAllUsers = async (req, res) => {
   }
 };
 
-
-
 module.exports = {
   registerUser,
   registerAdminFromDashboard,
   login,
-  getAllUsers
+  getAllUsers,
 };
---
+++ server/middlewares/validatePassword.js
@@ -0,0 +1,15 @@
+// middleware de validación de formato de contraseña
+const validatePassword = (req, res, next) => {
+    const { password } = req.body;
+    // Define una expresión regular para validar que la contraseña contenga al menos una minúscula, una mayúscula, un número y un caracter especial, y tenga al menos 8 caracteres en total
+    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+    if (!passwordRegex.test(password)) {
+      return res.status(400).json({ message: "La contraseña debe contener al menos una minúscula, una mayúscula, un número, un caracter especial y tener al menos 8 caracteres" });
+    }
+    next();
+  };
+  
+  module.exports = {
+    validatePassword
+  };
+  
