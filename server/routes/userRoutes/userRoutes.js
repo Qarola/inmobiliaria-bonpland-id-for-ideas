fee623f015f243d79526895a1d2e4dec993b6c68
Qarola <67078790+Qarola@users.noreply.github.com>
2024-05-12 19:54:07 -0300

    Update userRoutes.js

--- server/routes/userRoutes/userRoutes.js
+++ server/routes/userRoutes/userRoutes.js
@@ -2,12 +2,14 @@ const express = require('express');
 const router = express.Router();
 const { registerUser, registerAdminFromDashboard, login, getAllUsers } = require('../../controllers/userController/userController');
 const { validateToken, requireRole, createToken } = require('../../auth/Jwt.controller');
+const { validateEmail } = require('../../middlewares/validateEmail');
+const { validatePassword } = require('../../middlewares/validatePassword');
 
 //Ruta para traer todos los usuarios de la DB.
 router.get('/', getAllUsers);
 
 // Ruta para el registro de usuarios normales
-router.post('/register', registerUser);
+router.post('/register', validateEmail, validatePassword, registerUser);
 
 // Ruta para el registro de nuevos administradores desde el panel de administrador
 router.post('/admin/register', validateToken, requireRole('admin'), registerAdminFromDashboard);
