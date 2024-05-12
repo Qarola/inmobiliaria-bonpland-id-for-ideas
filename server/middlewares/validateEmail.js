53cacd4d28deee23ce336a59b8e724225a6b7e0e
Qarola <67078790+Qarola@users.noreply.github.com>
2024-05-12 19:53:02 -0300

    Update validateEmail.js

--- server/middlewares/validateEmail.js
+++ server/middlewares/validateEmail.js
@@ -1,11 +1,10 @@
 // middleware de validación de patrón de email
-const validateEmail = (req, res, next) => {
+const validateEmail = (req, res) => {
     const { email } = req.body;
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     if (!emailRegex.test(email)) {
       return res.status(400).json({ message: "Formato de correo electrónico inválido" });
     }
-    next();
   };
   
   module.exports = {
