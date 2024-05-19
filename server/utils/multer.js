d5809f64c23e134b579e8543e6bc605d78d387af
Qarola <cyan.323@gmail.com>
2024-05-19 14:29:17 -0300

    update multer

--- server/utils/multer.js
+++ server/utils/multer.js
@@ -1,27 +1,39 @@
 const multer = require('multer');
 const path = require('path');
 
-
-// Configuración de multer para el almacenamiento de archivos
+// Configuración de almacenamiento de multer
 const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-      cb(null, 'uploads/'); // Directorio donde se almacenarán las imágenes de perfil de usuario.
-    },
-    filename: (req, file, cb) => {
-      cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname)); // Nombre de archivo único
-    }
-  });
+  destination: (req, file, cb) => {
+    cb(null, 'uploads/'); // Directorio donde se almacenarán las imágenes
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname)); // Nombre de archivo único
+  }
+});
 
-// Configuración de multer para subir archivos
+// Filtro de archivos para permitir solo imágenes
+const fileFilter = (req, file, cb) => {
+  const filetypes = /jpeg|jpg|png/;
+  const mimetype = filetypes.test(file.mimetype);
+  const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+
+  if (mimetype && extname) {
+    return cb(null, true);
+  } else {
+    cb('Error: Las imágenes deben estar en formato JPEG, JPG o PNG');
+  }
+};
+
+// Configuración de multer 
 const propertyUpload = multer({
-    storage: storage,
-    limits: {
+  storage: storage,
+  limits: {
     fileSize: 1024 * 1024 * 2, // 2 MB (tamaño en bytes)
     files: 3 // Número máximo de archivos que se pueden cargar simultáneamente
-  }
-}); // 'images' es el nombre del campo de entrada del formulario donde se cargan las imágenes
+  },
+  fileFilter: fileFilter
+});
 
 module.exports = {
-    storage,
-    propertyUpload, 
-}
+  propertyUpload,
+};
