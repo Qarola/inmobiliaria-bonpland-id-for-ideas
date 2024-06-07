0db3cfdbf22852000ceb702c498c43928eb0fa4c
Qarola <67078790+Qarola@users.noreply.github.com>
2024-06-07 16:05:38 -0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -19,6 +19,7 @@ dotenv.config();
 const corsOptions = {
   origin: "http://localhost:5173",      /* "*", */
   methods: "GET,POST,PUT,DELETE",
+  credentials: true,
 };
 
 app.use(cors(corsOptions)); // Configura CORS
